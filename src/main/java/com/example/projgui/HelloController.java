d23f23b452c2c2ddccc20e20877691aab12f81ad
Myxhal <104581082+Myxhal@users.noreply.github.com>
2023-10-26 01:18:24 -0400

    Added text field and buttons to code.

--- src/main/java/com/example/projgui/HelloController.java
+++ src/main/java/com/example/projgui/HelloController.java
@@ -2,6 +2,12 @@ package com.example.projgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
@@ -13,4 +19,87 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    private TextField amountTwo;
+
+    @FXML
+    private TextField balanceField;
+
+    @FXML
+    private RadioButton cCheckingTwo;
+
+    @FXML
+    private RadioButton camdenOne;
+
+    @FXML
+    private RadioButton checkingButtonOne;
+
+    @FXML
+    private RadioButton checkingTwo;
+
+    @FXML
+    private Button clearButtonOne;
+
+    @FXML
+    private Button closeButtonOne;
+
+    @FXML
+    private RadioButton collegeCheckingButtonOne;
+
+    @FXML
+    private Button depositTwo;
+
+    @FXML
+    private TextField fNameFieldOne;
+
+    @FXML
+    private TextField fNameTwo;
+
+    @FXML
+    private CheckBox isLoyalOne;
+
+    @FXML
+    private TextField lNameFieldOne;
+
+    @FXML
+    private TextField lNameTwo;
+
+    @FXML
+    private Button loadAccountsThree;
+
+    @FXML
+    private RadioButton mMarketTwo;
+
+    @FXML
+    private RadioButton moneyMarketButtonOne;
+
+    @FXML
+    private RadioButton nbOne;
+
+    @FXML
+    private RadioButton newarkOne;
+
+    @FXML
+    private Button openButtonOne;
+
+    @FXML
+    private Button printAccThree;
+
+    @FXML
+    private Button printInterestThree;
+
+    @FXML
+    private RadioButton savingsButtonOne;
+
+    @FXML
+    private RadioButton savingsTwo;
+
+    @FXML
+    private TextArea tabOneTextArea;
+
+    @FXML
+    private Button updateAccountsThree;
+
+    @FXML
+    private Button withdrawlTwo;
 }
