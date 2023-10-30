5c73a613a3709244be760fa5d7c67364fbd94ce8
Myxhal <104581082+Myxhal@users.noreply.github.com>
2023-10-30 12:40:40 -0400

    Update

--- src/main/java/com/example/projgui/HelloController.java
+++ src/main/java/com/example/projgui/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.projgui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
@@ -10,9 +11,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.DatePicker;
 
+import java.util.Date;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private String firstName;
+    private String lastName;
+    private Date dob;
     //mychal
     //dennis
 
@@ -23,6 +29,23 @@ public class HelloController {
 
     @FXML
     private TextField fNameFieldOne, lNameFieldOne, balanceField;
+
+    String fName = fNameFieldOne.getText();
+
+
+
+    public void handle(ActionEvent event){
+        String fName = fNameFieldOne.getText();
+        String lName = lNameFieldOne.getText();
+        double intBalance = Integer.parseInt(balanceField.getText());
+        Date date = datePicker.g
+    }
+    public void Profile(String firstname, String lastName, Date dob) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.dob = dob;}
+
+
 
     @FXML
     private DatePicker datePicker;
@@ -51,4 +74,7 @@ public class HelloController {
 
     @FXML
     private Button printAccThree, loadAccountsThree, printInterestThree, updateAccountsThree;
+
+
+    }
 }
